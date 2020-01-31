b0e7e3a2a0f4038462acb890e80e31db2af96557
Brynjulf Risbakken <brynjulfz@hotmail.com>
2020-01-31 13:51:03 +0100

    expose the code that made the story (#51)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,6 +7,11 @@ module.exports = ({ config }) => {
       },
     ],
   });
+  config.module.rules.push({
+    test: /\.stories\.ts?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
   };
