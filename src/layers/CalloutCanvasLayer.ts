1803ccb1a29750b955560b53c8b690d91aa60e1b
Brynjulf <brisb@equinor.com>
2020-02-24 09:23:13 +0100

    added some initial text placement logic

--- src/layers/CalloutCanvasLayer.ts
+++ src/layers/CalloutCanvasLayer.ts
@@ -1,13 +1,20 @@
 import { ScaleLinear } from 'd3-scale';
+import { round } from '@equinor/videx-math';
 import { CanvasLayer } from './CanvasLayer';
 import { OnUpdateEvent, OnMountEvent, Annotation } from '../interfaces';
 
-import { calcTextSize } from '../utils';
+import { calcTextSize, positionCallout } from '../utils';
 
 const OFFSET : number = -20;
+const MAX_FONT_SIZE = 12;
+const MIN_FONT_SIZE = 7;
 
 export class CalloutCanvasLayer extends CanvasLayer {
   data : Annotation[] = [];
+  isPanning: boolean;
+  xRatio: number;
+  overlapped: any[];
+
   onMount(event : OnMountEvent) {
     super.onMount(event);
     this.data = event.annotations;
@@ -15,44 +22,84 @@ export class CalloutCanvasLayer extends CanvasLayer {
 
   onUpdate(event : OnUpdateEvent) {
     super.onUpdate(event);
-    this.render(event);
+    const {
+      xScale,
+      yScale,
+      scale,
+    } = event;
+    const isLeftToRight = false;
+
+    this.isPanning = this.xRatio && this.xRatio === event.xRatio;
+    this.xRatio = event.xRatio;
+
+    this.overlapped = positionCallout(this.data, isLeftToRight, xScale, yScale, scale, this.isPanning, this.overlapped, this.ctx);
+
+    this.render({ ...event, isLeftToRight });
   }
   render(event: OnUpdateEvent) {
+    const {
+      xScale,
+      yScale,
+    } = event;
+
     for (var i = 0; i < this.data.length; i++) {
       const {
         data,
         title,
       } = this.data[i];
-      const {
-        xScale,
-        yScale,
-      } = event;
       const x = xScale(data[0]);
       const y = yScale(data[1]);
 
-      this.renderText(xScale, x, y, title);
+      const anno = this.overlapped && this.overlapped[i];
+      const offsetX = anno ? anno.dx : OFFSET;
+      const offsetY = anno ? anno.dy : -OFFSET;
+
+      const label = (event.scale === 0 ?
+        `${round(anno.md)} ${anno.mdUnit} MD ${anno.depthReferencePoint}` :
+        `${round(anno.tvd)} ${anno.mdUnit} TVD MSL`);
+
+      const width =  Math.max(this.ctx.measureText(title).width, this.ctx.measureText(label).width);
+      const height = calcTextSize(12, MIN_FONT_SIZE, MAX_FONT_SIZE, xScale);
+
+      const color = anno.group === 'strat-picks' ?
+      '#227' :
+      'rgba(0,0,0,0.8)';
+
+      this.renderText(title, x - offsetX, y + offsetY - height, height, color);
+      this.renderText(label, x - offsetX, y + offsetY, height, color);
       this.renderPoint(x, y);
-      this.renderLines(x, y, title)
+      this.renderLines(x, y, width, offsetX, offsetY, color, event.isLeftToRight);
     }
   }
 
-  private renderText(xScale : ScaleLinear<number, number>, x : number, y : number, title : string) {
-    this.ctx.font =  `${calcTextSize(12, 7, 12, xScale)}px Arial`;
-    this.ctx.fillText(title, x - OFFSET, y + OFFSET);
+  private renderText(title : string, x : number, y : number, height: number, color : string) {
+    this.ctx.font =  `${height}px Arial`;
+    this.ctx.fillStyle = color;
+    this.ctx.fillText(title, x, y);
   }
 
   private renderPoint(x : number, y : number) {
     this.ctx.moveTo(x, y);
-    this.ctx.arc(x, y, 4, 0, Math.PI * 2);
+    this.ctx.arc(x, y, 2, 0, Math.PI * 2);
     this.ctx.fill();
   }
 
-  private renderLines(x : number, y : number, title : string) {
-    this.ctx.moveTo(x, y);
-    this.ctx.lineTo(x - OFFSET, y + OFFSET + 2);
-    this.ctx.moveTo(x - OFFSET, y + OFFSET + 2)
-    this.ctx.lineTo(x - OFFSET + this.ctx.measureText(title).width, y + OFFSET + 2);
-    this.ctx.lineWidth = 1;
-    this.ctx.stroke();
+  private renderLines(x : number, y : number, width : number, offsetX : number, offsetY : number, color : string, isLeftToRight : boolean) {
+    const { ctx } = this;
+    const textX = x - offsetX;
+    const textY = y + offsetY + 2;
+
+    const newX = x;
+    const newY = y;
+    ctx.moveTo(newX, newY);
+
+    ctx.lineTo(textX, textY);
+    ctx.moveTo(textX, textY);
+    ctx.lineTo(textX + width, textY);
+    ctx.moveTo(textX + width, textY);
+
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 1;
+    ctx.stroke();
   }
 };
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,4 +1,7 @@
-import { clamp } from '@equinor/videx-math';
+import { clamp, round } from '@equinor/videx-math';
+import { ScaleLinear } from 'd3-scale';
+
+import { Annotation } from '../interfaces';
 
 export const pixelsPerUnit = (x:  any) => {
   const [min] = x.domain();
@@ -7,4 +10,140 @@ export const pixelsPerUnit = (x:  any) => {
 
 export const calcTextSize = (factor : any, min: any, max: any, x: any) => {
   return clamp(pixelsPerUnit(x) * factor, min, max);
+}
+
+
+const getBoundingBox = (ctx :any , anno : Annotation, xScale : ScaleLinear<number, number>, yScale : ScaleLinear<number, number>, height: number, scale: number) => {
+  const ax1 = xScale(anno.data[0]);
+  const ay1 = yScale(anno.data[1]);
+
+  const titleWidth = ctx.measureText(anno.title).width;
+  const label = (scale === 0 ?
+    `${round(anno.md)} ${anno.mdUnit} MD ${anno.depthReferencePoint}` :
+    `${round(anno.tvd)} ${anno.mdUnit} TVD MSL`);
+  const labelWidth = ctx.measureText(label).width;
+  const width = Math.max(labelWidth, titleWidth);
+
+  const annotation = {
+    x: ax1,
+    y: ay1,
+    width,
+    height: (height * 2) + 4,
+  };
+  return annotation;
+}
+
+const checkForOverlap = (r1: any, r2: any) => {
+  const r1x2 = r1.x + r1.width;
+  const r2x2 = r2.x + r2.width;
+  const r1y2 = r1.y + r1.height;
+  const r2y2 = r2.y + r2.height;
+
+  if (r2.x > r1x2 || r2.y > r1y2 || r2x2 < r1.x || r2y2 < r1.y) {
+    return null;
+  }
+
+  const dx = Math.max(0, Math.min(r1.x + r1.width, r2.x + r2.width) - Math.max(r1.x, r2.x));
+  const dy = Math.max(0, Math.min(r1.y + r1.height, r2.y + r2.height) - Math.max(r1.y, r2.y));
+
+  const newPoints = {
+    dx,
+    dy,
+  };
+  return (newPoints);
+};
+
+const testTop = (p : any, n : any, isLeftToRight: boolean = false) => {
+  return isLeftToRight ? (n.x + n.width > p.x) : (p.x + p.width > n.x);
+}
+
+const testBottom = (p : any, n : any, isLeftToRight: boolean = false) => {
+    return isLeftToRight ? (n.x < p.x + p.width) : (p.x < n.x + n.width);
+}
+
+// calculates position of a list of annotations
+export const positionCallout = (
+    annotations : Annotation[],
+    isLeftToRight: boolean,
+    xScale : ScaleLinear<number, number>,
+    yScale : ScaleLinear<number, number>,
+    scale: number,
+    isPanning: boolean = false,
+    overlapped : any,
+    ctx: any,
+  ) => {
+
+  if (isPanning) {
+    return overlapped;
+  }
+  const offsetX = (isLeftToRight ? 20 : -20);
+  const offsetY = -20;
+
+  if (annotations.length < 2) return;
+  let nodes = annotations.map((a) => {
+    return {
+      ...a,
+      ...getBoundingBox(ctx, a, xScale, yScale, calcTextSize(12, 7, 12, xScale), scale),
+      dx: offsetX,
+      dy: offsetY,
+    };
+  });
+
+  const top = [nodes[nodes.length - 1]];
+  const bottom = [];
+  // initial best effort
+  for (let i = nodes.length - 2; i >= 0; i -= 1) {
+    const prevNode = nodes[i + 1];
+    const node = nodes[i];
+    const overlap = checkForOverlap(node, prevNode);
+
+    if (overlap) {
+      // flip to bottom
+      node.dy = -offsetY;
+      node.dx = offsetX;
+      node.x += (-2 * offsetX);
+      node.y += (-2 * offsetY);
+      bottom.push(node);
+      i -= 1; // skip next
+      if (i >= 0) {
+        top.push(nodes[i]);
+      }
+    } else {
+      top.push(node);
+    }
+  }
+
+  // adjust top
+  if (top.length > 1) {
+    for (let i = 1; i < top.length; i += 1) {
+      const node = top[i];
+      for (let j = 0; j < i; j += 1) {
+        const prevNode = top[j];
+        if (testTop(prevNode, node)) {
+          const overlap = checkForOverlap(node, prevNode);
+          if (overlap) {
+            node.dy -= overlap.dy;
+            node.y -= overlap.dy;
+          }
+        }
+      }
+    }
+  }
+  // adjust bottom
+  if (bottom.length > 1) {
+    for (let i = bottom.length - 2; i >= 0; i -= 1) {
+      const node = bottom[i];
+      for (let j = bottom.length - 1; j > i; j -= 1) {
+        const prevNode = bottom[j];
+        if (testBottom(prevNode, node)) {
+          const overlap = checkForOverlap(prevNode, node);
+          if (overlap) {
+            node.dy += overlap.dy;
+            node.y += overlap.dy;
+          }
+        }
+      }
+    }
+  }
+  return nodes;
 }
