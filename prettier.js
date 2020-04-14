5fb384e3e16bc9d466e04b76827f235cc21e1caf
Brynjulf <brisb@equinor.com>
2020-04-14 12:56:14 +0200

    added script to run prettier correctly on different os

---
+++ prettier.js
@@ -0,0 +1,24 @@
+var exec = require('child_process').exec;
+
+var os = require('os');
+
+var func = (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+      console.log(stderr);
+      return;
+  }
+  console.log(stdout);
+}
+
+if (os.type() === 'Linux')
+  exec("prettier --write --config .prettierrc '**/*.ts' ", func);
+else if (os.type() === 'Darwin')
+  exec("prettier --write --config .prettierrc '**/*.ts' ", func);
+else if (os.type() === 'Windows_NT')
+  exec("prettier --write --config .prettierrc **/*.ts ", func);
+else
+  throw new Error("Unsupported OS found: " + os.type());
