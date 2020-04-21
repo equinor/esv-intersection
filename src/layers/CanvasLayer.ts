f9a1b5aa3fa66a5e4d60ecc30e798f7e9b103d0f
Håvard Jakobsen <havard.n.jakobsen@lyse.net>
2020-04-21 09:00:57 +0200

    Support flipping the axis in canvas layer (#179)

--- src/layers/CanvasLayer.ts
+++ src/layers/CanvasLayer.ts
@@ -9,22 +9,26 @@ export abstract class CanvasLayer extends Layer {
 
   onOpacityChanged(opacity: number): void {
     if (this.canvas) {
-      this.canvas.setAttribute('style', `position:absolute;z-index:${this.order};opacity:${opacity}`);
+      this.setStyle(this.order, opacity, this.isVisible);
     }
   }
 
   onOrderChanged(order: number): void {
     if (this.canvas) {
-      this.canvas.setAttribute('style', `position:absolute;z-index:${order};opacity:${this.opacity}`);
+      this.setStyle(order, this.opacity, this.isVisible);
     }
   }
 
   setVisibility(visible: boolean): void {
     super.setVisibility(visible);
     if (this.canvas) {
-      const visibility = visible ? 'visible' : 'hidden';
-      this.canvas.setAttribute('style', `position:absolute;z-index:${this.order};opacity:${this.opacity};visibility:${visibility}`);
+      this.setStyle(this.order, this.opacity, visible);
     }
+  }
+
+  setStyle(opacity: number, order: number, visible: boolean = true): void {
+    const visibility = visible ? 'visible' : 'hidden';
+    this.canvas.setAttribute('style', `position:absolute;z-index:${this.order};opacity:${this.opacity};visibility:${visibility}`);
   }
 
   onMount(event: OnMountEvent): void {
@@ -40,10 +44,10 @@ export abstract class CanvasLayer extends Layer {
       event.elm.appendChild(canvas);
     }
     this.canvas.setAttribute('id', `${this.id}`);
-    this.canvas.setAttribute('style', `position:absolute;z-index:${this.order};opacity:${this.opacity}`);
     this.canvas.setAttribute('width', `${width}px`);
     this.canvas.setAttribute('height', `${height}px`);
     this.canvas.setAttribute('class', 'canvas-layer');
+    this.setStyle(this.order, this.opacity);
     this.ctx = this.canvas.getContext('2d');
   }
 
@@ -63,10 +67,6 @@ export abstract class CanvasLayer extends Layer {
 
   onUpdate(event: OnUpdateEvent): void {
     super.onUpdate(event);
-    const { ctx } = this;
-    if (!ctx) {
-      return;
-    }
   }
 
   resetTransform(): void {
@@ -75,8 +75,10 @@ export abstract class CanvasLayer extends Layer {
 
   setTransform(event: OnRescaleEvent): void {
     this.resetTransform();
-    this.ctx.translate(event.transform.x, event.transform.y);
-    this.ctx.scale(event.xRatio, event.yRatio);
+    const flippedX = event.xBounds[0] > event.xBounds[1];
+    const flippedY = event.yBounds[0] > event.yBounds[1];
+    this.ctx.translate(event.xScale(0), event.yScale(0));
+    this.ctx.scale(event.xRatio * (flippedX ? -1 : 1), event.yRatio * (flippedY ? -1 : 1));
   }
 
   clearCanvas(): void {
