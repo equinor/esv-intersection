09f6ee74f0b96872d016fa400f440beefa508157
Brynjulf Risbakken <brisb@statoil.com>
2020-06-09 12:51:59 +0200

    removed unused example data, also removed reference to non-existing files (#321)
    Co-authored-by: Brynjulf <brisb@equinor.com>

--- .storybook/src/exampledata/index.ts
+++ .storybook/src/exampledata/index.ts
@@ -1,14 +1,1 @@
-// mocked data
-export * as annotations from './annotations.json';
-export * as holeSizeData from './holesize.json';
-export * as casingData from './casings.json';
-export * as cementData from './cement.json';
 export * as seismicColorMap from './seismic-colormap.json';
-export * as poslog from './poslog.json';
-export * as completionData from './mock/completion.json';
-export * as stratcolumn from './stratcolumn.json';
-export * as picks from './picks.json';
-export * as casing from './casing.json';
-export * as completion from './completion.json';
-export * as seismic from './seismic.json';
-export * as surfaces from './surfaces.json';
