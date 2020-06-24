90d806d1f8f6af0304b72dfef033d42e7eab058f
Brynjulf <brisb@equinor.com>
2020-06-24 14:39:35 +0200

    ran prettier, and removed commented code

--- src/utils/vectorUtils.ts
+++ src/utils/vectorUtils.ts
@@ -8,7 +8,6 @@ export const vectorToArray = (v: Vector2): [number, number] => [v[0], v[1]];
 export const arrayToPoint = (a: [number, number]): Point => new Point(a[0], a[1]);
 export const arrayToVector = (a: [number, number]): Vector2 => new Vector2(a[0], a[1]);
 
-
 export const calcDist = (prev: [number, number], point: [number, number]): number => {
   return arrayToVector(point).sub(prev).magnitude;
 };
@@ -34,18 +33,17 @@ export const createNormal = (coords: Point[], offset: number): Point[] => {
 
   for (let i = 0; i < coords.length - nextToLastPointIndex; i++) {
     const p = pointToVector(coords[i]);
-    const n = pointToVector(coords[i+1]).sub(p).rotate90().normalize()
+    const n = pointToVector(coords[i + 1])
+      .sub(p)
+      .rotate90()
+      .normalize();
     newPoints.push(vectorToPoint(p.add(n.scale(offset))));
   }
   if (coords.length > nextToLastPointIndex) {
     const p = pointToVector(coords[coords.length - nextToLastPointIndex]);
     const q = pointToVector(coords[coords.length - nextToLastPointIndex - 1]);
-    const n = p.sub(q).rotate90().normalize()
+    const n = p.sub(q).rotate90().normalize();
     newPoints.push(vectorToPoint(p.add(n.scale(offset))));
   }
   return newPoints;
 };
-
-// getAngle(p1: { y: number; x: number }, p2: { y: number; x: number }): number {
-//   return Math.atan2(p2.y - p1.y, p2.x - p1.x);
-// }
