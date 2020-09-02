9d1ac062237f9be95f8ebd4756bf90b3eba095d1
Oystein Lygre <oystein.lygre@knowit.no>
2020-09-02 10:41:43 +0200

    update storybook to 6.0.21
    - Update storybook configuration to new "0-config" setup
    - Update storybook-dark-mode
    - Add @storybook/addon-docs to get @storybook/addon-storysource working
    
    @storybook/addon-storysource wouldn't find the source files and only way I found to get it working was adding @storybook/addon-docs

--- .storybook/config.js
+++
@@ -1,4 +0,0 @@
-
-import { configure } from '@storybook/html';
-
-configure(require.context('./src', true, /\.stories\.ts$/), module);
---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../.storybook/src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-storysource', 'storybook-dark-mode'],
+};
