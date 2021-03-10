966d489ab59c5641d90406d3c8371a4003a05f1e
Håvard Jakobsen <havard.n.jakobsen@lyse.net>
2021-03-10 15:41:36 +0100

    Add option for using a optional range for generating seismic image. (#425)

--- src/datautils/seismicimage.ts
+++ src/datautils/seismicimage.ts
@@ -4,18 +4,25 @@ import { clamp } from '@equinor/videx-math';
 import { createColorTable } from './colortable';
 import { findIndexOfSample } from './findsample';
 
-export function getSeismicInfo(seismic: any, trajectory: number[][]): any {
-  if (!(seismic && seismic.datapoints)) {
+/**
+ * Get key information about the seismic data
+ * Code originally developed for the REP project
+ * @param data Seismic data
+ * @param trajectory Wellbore or freehand trajectory
+ * @return  Key domain and depth information for seismic data
+ */
+export function getSeismicInfo(data: { datapoints: number[][]; yAxisValues: number[] }, trajectory: number[][]): any {
+  if (!(data && data.datapoints)) {
     return;
   }
   const minX = trajectory.reduce((acc: number, val: number[]) => Math.min(acc, val[0]), 0);
   const maxX = trajectory.reduce((acc: number, val: number[]) => Math.max(acc, val[0]), 0);
 
-  const minTvdMsl = seismic.yAxisValues && seismic.yAxisValues[0];
-  const maxTvdMsl = seismic.yAxisValues && seismic.yAxisValues[seismic.yAxisValues.length - 1];
+  const minTvdMsl = data.yAxisValues && data.yAxisValues[0];
+  const maxTvdMsl = data.yAxisValues && data.yAxisValues[data.yAxisValues.length - 1];
 
   // Find value domain
-  const dp = seismic.datapoints || [];
+  const dp = data.datapoints || [];
   const min = -dp.reduce((val: number, array: number[]) => Math.min(...array, val), 0);
   const max = dp.reduce((val: number, array: number[]) => Math.max(...array, val), 0);
 
@@ -39,19 +46,36 @@ export function getSeismicInfo(seismic: any, trajectory: number[][]): any {
   return info;
 }
 
+/**
+ * Generate seismic
+ * Code originally developed for the REP project
+ * @param data Seismic data
+ * @param trajectory Wellbore or freehand trajectory
+ * @param colormap Color map for rendering
+ * @param options.isLeftToRight (optional) draw left to right
+ * @param options.seismicRange (optional) Range for mapping seimic values to color map
+ * @param options.seismicMin (optional) Min seismic value for mapping seimic values to color map
+ * @param options.seismicMax (optional) Max seismic value for mapping seimic values to color map
+ * @return  Key domain and depth information for seismic data
+ */
 export async function generateSeismicSliceImage(
   data: { datapoints: number[][]; yAxisValues: number[] },
   trajectory: number[][],
   colormap: string[],
-  isLeftToRight = true,
+  options: {
+    isLeftToRight: true;
+    seismicRange?: number;
+    seismicMin?: number;
+    seismicMax?: number;
+  },
 ): Promise<ImageBitmap> {
   if (!(data && data.datapoints && data.datapoints.length > 0)) {
     return;
   }
   const { datapoints: dp } = data;
 
-  const min = dp.reduce((val: number, array: number[]) => Math.min(...array, val), 0);
-  const max = dp.reduce((val: number, array: number[]) => Math.max(...array, val), 0);
+  const min = options?.seismicMin || options?.seismicRange || dp.reduce((val: number, array: number[]) => Math.min(...array, val), 0);
+  const max = options?.seismicMax || options?.seismicRange || dp.reduce((val: number, array: number[]) => Math.max(...array, val), 0);
 
   const absMax = Math.max(Math.abs(min), Math.abs(max));
 
@@ -78,9 +102,9 @@ export async function generateSeismicSliceImage(
   let offset = 0;
   const colorFactor = (steps - 1) / domain.difference;
 
-  let pos = isLeftToRight ? trajectory[0][0] : trajectory[trajectory.length - 1][0];
+  let pos = options?.isLeftToRight ? trajectory[0][0] : trajectory[trajectory.length - 1][0];
 
-  const step = (length / width) * (isLeftToRight ? -1 : 1);
+  const step = (length / width) * (options?.isLeftToRight ? -1 : 1);
 
   let val1;
   let val2;
