d63ea47bde7888d6c18955e065eb0b6205f7e4bc
Ovin Eilertsen Teige <ovit@equinor.com>
2022-09-19 18:47:14 +0200

    Add types to picks.ts

--- src/datautils/picks.ts
+++ src/datautils/picks.ts
@@ -1,81 +1,115 @@
 import { Annotation } from '../interfaces';
 
-function mapPick(
-  p: { pickIdentifier: any; identifier: any; md: number; tvd: any; mdUnit: any; depthReferencePoint: any },
-  groupName: string,
-): Annotation {
-  const itm = {
-    title: p.pickIdentifier || p.identifier,
-    group: groupName,
-    label: `${p.md} ${p.mdUnit} ${p.depthReferencePoint}`,
-    color: groupName === 'strat-picks' ? '#227' : 'rgba(0,0,0,0.8)',
-    md: p.md,
-  };
-  return itm;
-}
+type Pick = {
+  pickIdentifier?: string;
+  confidence: string | null;
+  depthReferencePoint: string;
+  md: number;
+  mdUnit: string;
+  tvd: number;
+};
 
-function getReferencePicks(picks: any[]): any[] {
+type PickWithId = {
+  identifier: string;
+} & Pick;
+
+type Unit = {
+  identifier: string;
+  top: string;
+  base: string;
+  baseAge: number;
+  topAge: number;
+  colorR: number;
+  colorG: number;
+  colorB: number;
+  stratUnitLevel: number;
+  lithologyType: number;
+  stratUnitParent: number;
+};
+
+type UnitDto = {
+  unitName: string;
+  topSurface: string;
+  baseSurface: string;
+  ageBase: number;
+  ageTop: number;
+  color: {
+    r: number;
+    g: number;
+    b: number;
+  };
+  level: number;
+  lithType: number;
+  parent: number;
+};
+
+type PickAndUnit = PickWithId & UnitDto;
+
+type PairedPickAndUnit = {
+  name: string;
+  mdEntry: number;
+  tvdEntry: number;
+  color: { r: number; g: number; b: number };
+  level: number;
+  entryPick: PickAndUnit;
+  mdExit: number;
+  tvdExit: number;
+  exitPick: PickAndUnit;
+  confidenceEntry: string;
+  confidenceExit: string;
+  from?: number;
+  to?: number;
+};
+
+const mapPick = (p: PickWithId, groupName: string): Annotation => ({
+  title: p.pickIdentifier || p.identifier,
+  group: groupName,
+  label: `${p.md} ${p.mdUnit} ${p.depthReferencePoint}`,
+  color: groupName === 'strat-picks' ? '#227' : 'rgba(0,0,0,0.8)',
+  md: p.md,
+});
+
+function getReferencePicks(picks: PickWithId[]): Annotation[] {
   if (!picks) {
     return [];
   }
 
-  return picks.map((p: any) => mapPick(p, 'ref-picks'));
+  return picks.map((p: PickWithId) => mapPick(p, 'ref-picks'));
 }
 
-function getEntryPicks(formationPicks: any[]): any[] {
+function getEntryPicks(formationPicks: PairedPickAndUnit[]): Annotation[] {
   if (!formationPicks) {
     return [];
   }
 
   return formationPicks
-    .filter((d: { entryPick: { md: any }; from: any }) => d.entryPick.md === d.from)
-    .map((p: { entryPick: any }) => mapPick(p.entryPick, 'strat-picks'));
+    .filter((d: PairedPickAndUnit) => d.entryPick.md === d.from)
+    .map((p: PairedPickAndUnit) => mapPick(p.entryPick, 'strat-picks'));
 }
 
-function getFilteredExitPicks(formationPicks: any[]): any[] {
+function getFilteredExitPicks(formationPicks: PairedPickAndUnit[]): Annotation[] {
   if (!formationPicks) {
     return [];
   }
 
   return (
     formationPicks
-      .filter(
-        (d: { exitPick: { md: number } }) =>
-          formationPicks.findIndex((p: { entryPick: { md: number } }) => Math.abs(p.entryPick.md - d.exitPick.md) < 0.5) === -1,
-      )
-      .map((p: { exitPick: any }) => mapPick(p.exitPick, 'strat-picks'))
+      .filter((d: PairedPickAndUnit) => formationPicks.findIndex((p: PairedPickAndUnit) => Math.abs(p.entryPick.md - d.exitPick.md) < 0.5) === -1)
+      .map((p: PairedPickAndUnit) => mapPick(p.exitPick, 'strat-picks'))
       // Remove duplicates from unitpicks filling in gaps in formation
-      .filter(
-        (obj: Annotation, i: any, array: any[]) => i === array.findIndex((v: { title: any; md: any }) => v.title === obj.title && v.md === obj.md),
-      )
+      .filter((obj: Annotation, i: number, array: Annotation[]) => i === array.findIndex((v: Annotation) => v.title === obj.title && v.md === obj.md))
   );
 }
 
-export function getPicksData(picksData: any): any[] {
-  const { unitPicks, nonUnitPicks } = picksData;
-
-  const picks = [...getReferencePicks(nonUnitPicks), ...getEntryPicks(unitPicks), ...getFilteredExitPicks(unitPicks)];
-
-  picks.sort((a, b) => a.md - b.md);
-
-  return picks;
-}
+export const getPicksData = (picksData: { unitPicks: PairedPickAndUnit[]; nonUnitPicks: PickWithId[] }): Annotation[] =>
+  [...getReferencePicks(picksData.nonUnitPicks), ...getEntryPicks(picksData.unitPicks), ...getFilteredExitPicks(picksData.unitPicks)].sort(
+    (a, b) => a.md - b.md,
+  );
 
 /**
- * @param {{
- *  identifier: string,
- *  top: string,
- *  base: string,
- *  baseAge: number,
- *  topAge: number,
- *  colorR: number,
- *  colorG: number,
- *  colorB: number,
- *  stratUnitLevel: number,
- *  lithologyType: number,
- *  stratUnitParent: number }} u
+ * @param {Unit} u
  */
-const unitDto = (u: any) => ({
+const unitDto = (u: Unit): UnitDto => ({
   unitName: u.identifier,
   topSurface: u.top,
   baseSurface: u.base,
@@ -95,16 +129,16 @@ const unitDto = (u: any) => ({
  *
  * @param {number} from
  * @param {number} to
- * @param {object[]} arr
+ * @param {{ from: number; to: number; itm: PairedPickAndUnit }[]} arr
  * @param {number} arr.to
  * @param {number} arr.from
  * @returns {[number, number][]}
  */
-function findGaps(from: number, to: number, arr: any[]) {
+function findGaps(from: number, to: number, arr: { from: number; to: number; itm: PairedPickAndUnit }[]): [number, number][] {
   if (arr.length === 0) {
     return [[from, to]];
   }
-  const gaps = [];
+  const gaps: [number, number][] = [];
   let d = from;
   let i = 0;
   while (d < to && i < arr.length) {
@@ -122,31 +156,24 @@ function findGaps(from: number, to: number, arr: any[]) {
 }
 
 /**
- * @param {[]} units
- * @param {[]} res
- * @returns {[]}
+ * @param {Unit[]} units
+ * @returns {UnitDto[]}
  */
-function transformStratColumn(units: any, res: any[] = []) {
-  for (let i = 0; i < units.length; i += 1) {
-    const unit = unitDto(units[i]);
-    res.push(unit);
-  }
-  return res;
-}
+const transformStratColumn = (units: Unit[]): UnitDto[] => units.map(unitDto);
 
 /**
  * Join picks data with strat column units
- * @param {object[]} picks picks
- * @param {object[]} stratColumn strat column
+ * @param {Pick[]} picks picks
+ * @param {Unit[]} stratColumn strat column
  */
-function joinPicksAndStratColumn(picks: any, stratColumn: any) {
+function joinPicksAndStratColumn(picks: Pick[], stratColumn: Unit[]): { joined: PickAndUnit[]; nonUnitPicks: Pick[] } {
   const transformed = transformStratColumn(stratColumn);
-  const nonUnitPicks: any[] = [];
-  const joined: any[] = [];
-  picks.forEach((p: any) => {
-    const matches = transformed.filter((u) => p.pickIdentifier.search(new RegExp(`(${u.topSurface}|${u.baseSurface})`, 'i')) !== -1);
+  const nonUnitPicks: Pick[] = [];
+  const joined: PickAndUnit[] = [];
+  picks.forEach((p: Pick) => {
+    const matches = transformed.filter((u: UnitDto) => p.pickIdentifier.search(new RegExp(`(${u.topSurface}|${u.baseSurface})`, 'i')) !== -1);
     if (matches.length > 0) {
-      matches.forEach((u) =>
+      matches.forEach((u: UnitDto) =>
         joined.push({
           md: p.md,
           tvd: p.tvd,
@@ -167,21 +194,21 @@ function joinPicksAndStratColumn(picks: any, stratColumn: any) {
 
 /**
  * Find matching pairs of entry/exit picks
- * @param {object[]} joined picks joined with strat column units
+ * @param {PickAndUnit[]} joined picks joined with strat column units
  */
-function pairJoinedPicks(joined: any) {
+function pairJoinedPicks(joined: PickAndUnit[]): PairedPickAndUnit[] {
   // pair picks by unit name
   const pairs = [];
   let current = null;
 
   const sorted = joined
-    .filter((d: any) => d.level)
-    .sort((a: any, b: any) => a.unitName.localeCompare(b.unitName) || a.md - b.md || a.ageTop - b.ageTop);
+    .filter((d: PickAndUnit) => d.level)
+    .sort((a: PickAndUnit, b: PickAndUnit) => a.unitName.localeCompare(b.unitName) || a.md - b.md || a.ageTop - b.ageTop);
 
   while (sorted.length > 0) {
     current = sorted.shift();
     const name = current.identifier;
-    let pairWithName: any;
+    let pairWithName: string;
 
     const isTop = name === current.topSurface;
     const isBase = name === current.baseSurface;
@@ -195,18 +222,18 @@ function pairJoinedPicks(joined: any) {
       continue;
     }
 
-    let top: any;
-    let base: any;
+    let top: PickAndUnit;
+    let base: PickAndUnit;
 
-    const pairWith = sorted.find((p: any) => p.identifier === pairWithName);
+    const pairWith = sorted.find((p: PickAndUnit) => p.identifier === pairWithName);
     if (!pairWith) {
       console.warn(`Unable to find ${pairWithName} pick for ${name}`);
       if (isTop) {
         top = current;
         base = joined
-          .filter((d: any) => d.level)
-          .sort((a: any, b: any) => a.md - b.md)
-          .find((p: any) => p.md > top.md);
+          .filter((d: PickAndUnit) => d.level)
+          .sort((a: PickAndUnit, b: PickAndUnit) => a.md - b.md)
+          .find((p: PickAndUnit) => p.md > top.md);
         if (base) {
           console.warn(`Using ${base.identifier} as base for ${name}`);
         } else {
@@ -216,9 +243,9 @@ function pairJoinedPicks(joined: any) {
       } else if (isBase) {
         base = current;
         top = joined
-          .filter((d: any) => d.level)
-          .sort((a: any, b: any) => b.md - a.md)
-          .find((p: any) => p.md < base.md);
+          .filter((d: PickAndUnit) => d.level)
+          .sort((a: PickAndUnit, b: PickAndUnit) => b.md - a.md)
+          .find((p: PickAndUnit) => p.md < base.md);
         if (top) {
           console.warn(`Using ${top.identifier} as top for ${name}`);
         } else {
@@ -240,7 +267,7 @@ function pairJoinedPicks(joined: any) {
       sorted.splice(sorted.indexOf(pairWith), 1);
     }
 
-    pairs.push({
+    pairs.push(<PairedPickAndUnit>{
       name: top.unitName,
       mdEntry: top.md,
       tvdEntry: top.tvd,
@@ -260,15 +287,15 @@ function pairJoinedPicks(joined: any) {
 
 /**
  * Transform data for formation track
- * @param {object[]} picks picks
- * @param {object[]} stratColumn strat column
+ * @param {Pick[]} picks picks
+ * @param {Unit[]} stratColumn strat column
  */
-export function transformFormationData(picks: any, stratColumn: any) {
+export function transformFormationData(picks: Pick[], stratColumn: Unit[]): { unitPicks: PairedPickAndUnit[]; nonUnitPicks: Pick[] } {
   const { joined, nonUnitPicks } = joinPicksAndStratColumn(picks, stratColumn);
   const pairs = pairJoinedPicks(joined);
 
   const itemstack = pairs
-    .filter((d) => d.mdEntry < d.mdExit)
+    .filter((d: PairedPickAndUnit) => d.mdEntry < d.mdExit)
     .sort((a, b) => a.mdEntry - b.mdEntry || a.level - b.level)
     .reverse();
 
@@ -277,14 +304,14 @@ export function transformFormationData(picks: any, stratColumn: any) {
   const unitPicks = [];
   while (itemstack.length > 0) {
     const first = itemstack.pop();
-    const group = [];
+    const group: PairedPickAndUnit[] = [];
     while (itemstack.length > 0 && itemstack[itemstack.length - 1].level > first.level) {
       group.push(itemstack.pop());
     }
     group.reverse();
     group.push(first);
-    const arr: any[] = [];
-    group.forEach((itm) => {
+    const arr: { from: number; to: number; itm: PairedPickAndUnit }[] = [];
+    group.forEach((itm: PairedPickAndUnit) => {
       const gaps = findGaps(itm.mdEntry, itm.mdExit, arr);
       arr.push(...gaps.map((g) => ({ from: g[0], to: g[1], itm })));
     });
