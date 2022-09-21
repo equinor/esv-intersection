1a26f70a7abeeea729e9530b9105dc0c1db51106
Ovin Eilertsen Teige <ovit@equinor.com>
2022-09-21 09:57:22 +0200

    Remove casting from extended curve interpolator

--- src/control/ExtendedCurveInterpolator.ts
+++ src/control/ExtendedCurveInterpolator.ts
@@ -148,8 +148,8 @@ export class ExtendedCurveInterpolator extends CurveInterpolator {
     return this.getPointAt(t);
   }
 
-  getPointAt(t: number): number[] {
+  getPointAt(t: number): Vector {
     const tl = clamp(t, 0, 1);
-    return super.getPointAt(tl) as number[];
+    return super.getPointAt(tl);
   }
 }
