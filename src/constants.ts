1defb0ce1b66a10c93695342fdb265e2469a2120
Øystein Lygre <oystein.lygre@knowit.no>
2022-09-30 09:39:32 +0200

    feat: prepare for < 1 scaling factor (#496)

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const DEFAULT_LAYER_HEIGHT = 300;
 export const HORIZONTAL_AXIS_MARGIN = 40;
 export const VERTICAL_AXIS_MARGIN = 30;
 
-export const HOLE_OUTLINE = 1;
+export const HOLE_OUTLINE = 0.5;
 export const SHOE_WIDTH = 25;
 export const SHOE_LENGTH = 12;
 
