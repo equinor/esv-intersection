7eefce4b4eff63877a112f29a9c2a41d3adef2b4
Ovin Eilertsen Teige <ovit@equinor.com>
2022-10-20 11:07:01 +0200

    Rename and add some more color tests

--- test/utils/color.test.ts
+++ test/utils/color.test.ts
@@ -20,19 +20,35 @@ describe('color', () => {
   });
 
   describe('colorToCSSColor', () => {
-    it('should css color string', () => {
+    it('should convert to css color string', () => {
       const convertedColor = colorToCSSColor(13089195);
       expect(convertedColor).toEqual('#c7b9ab');
     });
 
-    it('should convert black css name alias', () => {
+    it('should convert to black hex string', () => {
       const convertedColor = colorToCSSColor(0);
       expect(convertedColor).toEqual('#000000');
     });
 
-    it('should convert white css name alias', () => {
+    it('should convert to white hex string', () => {
       const convertedColor = colorToCSSColor(16777215);
       expect(convertedColor).toEqual('#ffffff');
     });
+
+    it('should convert to hex number to hex string', () => {
+      const convertedColor = colorToCSSColor(0xffffff);
+      expect(convertedColor).toEqual('#ffffff');
+    });
+
+    it('should pad short hex number to hex string', () => {
+      const convertedColor = colorToCSSColor(0xfff);
+      expect(convertedColor).toEqual('#000fff');
+    });
+  });
+
+  it('should convert colors back and forth', () => {
+    const number = 123;
+    const cssColor = colorToCSSColor(number);
+    expect(convertColor(cssColor)).toEqual(number);
   });
 });
