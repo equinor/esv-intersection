6f909e75db5125adc37f0608f5146670ada6d94f
Ovin Eilertsen Teige <ovit@equinor.com>
2022-10-20 11:21:00 +0200

    Update the test with hex numbers

--- test/utils/color.test.ts
+++ test/utils/color.test.ts
@@ -3,39 +3,39 @@ import { convertColor, colorToCSSColor } from '../../src/utils/color';
 
 describe('color', () => {
   describe('convertColor', () => {
-    it('should css color string', () => {
+    it('should convert long form css color string to number', () => {
       const convertedColor = convertColor('#c7b9ab');
-      expect(convertedColor).toEqual(13089195);
+      expect(convertedColor).toEqual(0xc7b9ab);
+    });
+
+    it('should convert short hand css color to number', () => {
+      const convertedColor = convertColor('#abc');
+      expect(convertedColor).toEqual(0xaabbcc);
     });
 
     it('should convert black css name alias', () => {
       const convertedColor = convertColor('black');
-      expect(convertedColor).toEqual(0);
+      expect(convertedColor).toEqual(0x0);
     });
 
     it('should convert white css name alias', () => {
       const convertedColor = convertColor('white');
-      expect(convertedColor).toEqual(16777215);
+      expect(convertedColor).toEqual(0xffffff);
     });
   });
 
   describe('colorToCSSColor', () => {
     it('should convert to css color string', () => {
-      const convertedColor = colorToCSSColor(13089195);
+      const convertedColor = colorToCSSColor(0xc7b9ab);
       expect(convertedColor).toEqual('#c7b9ab');
     });
 
     it('should convert to black hex string', () => {
-      const convertedColor = colorToCSSColor(0);
+      const convertedColor = colorToCSSColor(0x0);
       expect(convertedColor).toEqual('#000000');
     });
 
     it('should convert to white hex string', () => {
-      const convertedColor = colorToCSSColor(16777215);
-      expect(convertedColor).toEqual('#ffffff');
-    });
-
-    it('should convert to hex number to hex string', () => {
       const convertedColor = colorToCSSColor(0xffffff);
       expect(convertedColor).toEqual('#ffffff');
     });
@@ -47,7 +47,7 @@ describe('color', () => {
   });
 
   it('should convert colors back and forth', () => {
-    const number = 123;
+    const number = 12345678;
     const cssColor = colorToCSSColor(number);
     expect(convertColor(cssColor)).toEqual(number);
   });
