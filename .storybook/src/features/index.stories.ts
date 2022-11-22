fe2c81adf131b30446f7b117471ba6700bdc2876
Ovin Eilertsen Teige <91891939+ovineq@users.noreply.github.com>
2022-11-22 09:34:44 +0100

    chore:  Upgrade packages & remove stories related to deleted layers (#521)
    Use schematiclayer instead

--- .storybook/src/features/index.stories.ts
+++ .storybook/src/features/index.stories.ts
@@ -5,12 +5,8 @@ export * from './grid-layer.stories';
 export * from './seismic.stories';
 export * from './highlight.stories';
 export * from './wellborepath-layer.stories';
-export * from './casing-layer.stories';
-export * from './cement-layer.stories';
-export * from './holesize-layer.stories';
 export * from './geomodel-layer.stories';
 export * from './schematic-layer.stories';
-export * from './casing-and-cement.stories';
 
 export default {
   title: 'Features',
