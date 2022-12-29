93b78c773349e11eb91b9837618290a653949335
Oystein Lygre <oystein.lygre@knowit.no>
2022-12-29 10:53:57 +0100

    remove wrong build

--- storybook/)/assets/index.stories.57843f77.js
+++
@@ -1,2 +0,0 @@
-import{BasicSetup as a}from"./basic-intersection.stories.582e0b00.js";import"./elements.87f68392.js";import"./_commonjsHelpers.c10bf6cb.js";import"./GridLayer.439d0b2f.js";import"./MainController.5bd28a66.js";const p={title:"Other examples"},i=[];export{a as BasicSetup,i as __namedExportsOrder,p as default};
-//# sourceMappingURL=index.stories.57843f77.js.map
--- storybook/)/control/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './interfaces';
-export * from './IntersectionReferenceSystem';
-export * from './LayerManager';
-export * from './MainController';
-export * from './ZoomPanHandler';
