0c0ce2662e0d93a133245206d59c7e1c37cc3d2f
VRiveland <vriv@equinor.com>
2025-09-05 09:53:18 +0200

    Removed storybook-dark-mode as it won't work with storybook v9

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../.storybook/src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['storybook-dark-mode'],
+  addons: [],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/html-vite',
