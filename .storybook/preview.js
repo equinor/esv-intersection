649e87d04a701f346c63bf7346b36c00b5ab553f
VRiveland <vriv@equinor.com>
2025-10-27 09:42:51 +0100

    Make the code look decent

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,18 @@ const preview = {
   parameters: {
     docs: {
       codePanel: true,
+      source: {
+        transform: async (source) => {
+          const prettier = await import('prettier/standalone');
+          const prettierPluginBabel = await import('prettier/plugins/babel');
+          const prettierPluginEstree = await import('prettier/plugins/estree');
+
+          return prettier.format(source, {
+            parser: 'babel',
+            plugins: [prettierPluginBabel, prettierPluginEstree],
+          });
+        },
+      },
     },
   },
 };
